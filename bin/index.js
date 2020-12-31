e7b073bd4c7da899bcba1bcdea274008181b0b23
Hrishit Chaudhuri <hrishitchaudhuri@gmail.com>
2020-12-31 10:13:41 +0530

    Added final methods

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,7 @@ for (let e in argv) {
 }
 
 if (cnt == 2) {
-    const errMsg = chalk.red.bold("[ERR] Please enter a valid argument.")
+    const errMsg = chalk.red.bold(">> [ERR] Please enter a valid argument. <<")
     console.log(errMsg);
     process.exit(1);
 }
@@ -27,19 +27,12 @@ const boxenOptions = {
     backgroundColor: "green"
 };
 
-/*
-const srchOptions = yargs
-    .option("get-note", { alias: "getnotebytitle", describe: "Get all tasks under a common title", type: "string" })
-    .option("make-note", { alias: "makenotebytitle", describe: "Upload a note", type: "string" })
-    .argv;
-*/
-
 let url = 'http://localhost:8000/mapi/'
 
 
 // Obtain: Searches for data by title.
-if (argv.obtain) {
-    url += `search/?TITLE=${escape(argv.gt)}`;
+if (argv.obtain && argv.t) {
+    url += `search/?TITLE=${escape(argv.t)}`;
 
     axios.get(url, { headers: { Accept: "application/json" }})
         .then(res => {
@@ -91,8 +84,8 @@ if (argv.recite) {
         });
 }
 
-// Concoct: Creates a new note with a new note ID. Also requires the -t and -m flags to set title and message. 
-if (argv.concoct && argv.t && argv.m) {
+// Forge: Creates a new note with a new note ID. Also requires the -t and -m flags to set title and message. 
+if (argv.forge && argv.t && argv.m) {
     var stats_url = url + 'stats';
 
     axios.get(stats_url)
@@ -108,27 +101,47 @@ if (argv.concoct && argv.t && argv.m) {
                 TITLE: argv.t
             }
 
-            axios.post(url, note);
+            axios.post(url, note, {
+                headers: {
+                    "Content-type" : "application/json",
+                    "Access-Control-Allow-Origin" : "*"
+                }
+            })
+                .then(response => {
+                    const successMsg = chalk.green.bold(">> [INFO] FORGE successful. <<");
+                    console.log(successMsg);
+                });
         });
-
 }
 
 // Obliterate: Clear notes by ID.
-if (argv.obliterate) {
+if (argv.oblit) {
+    url += 'delete';
 
+    const note = {
+        NOTE_ID : argv.oblit
+    };
+
+    axios.post(url, note)
+        .then(res => {
+            const successMsg = chalk.green.bold(">> [INFO] OBLIT successful. <<");
+            console.log(successMsg);
+        });
 }
 
 // Affix: Add tasks to notes by ID.
-if (argv.affix) {
+if (argv.affix && argv.m) {
+    let get_url = url + `?NOTE_ID=${escape(argv.affix)}`;
 
-}
+    axios.get(get_url)
+        .then(res => {
+            let data = res.data;
+            data.ITEMS.push(argv.m);
 
-// Decimate: Delete all notes by title.
-if (argv.decimate) {
-
-}
-
-// Pulverize: Clear all notes (restart session).
-if (argv.pulverize) {
-
+            axios.put(url + '/affix', { NOTE_ID : argv.affix, ITEMS : data.ITEMS })
+                .then(res => {
+                    const successMsg = chalk.green.bold(">> [INFO] AFFIX successful. <<");
+                    console.log(successMsg);
+                });
+        });
 }
